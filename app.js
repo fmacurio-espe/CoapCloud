a2f8800e3bf6275a0d12060c44179b28d43740ee
fmacurio-espe <fmacurio@espe.edu.ec>
2019-02-06 02:32:53 -0500

    Comm

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const coap = require("node-coap-client").CoapClient;
 var hostname="localhost";
 var params={
     psk: { 
-        "identity": "12345678-1234-1234-1234-123456789012"
+        "identity": "AAAAAAAAAAAAAAAA"
         // ... potentially more psk identities
     }
 };
@@ -12,22 +12,42 @@ coap
     .tryToConnect(target /* string */)
     .then((result /* true or error code or Error instance */) => {
         // do something with the result */ 
+        console.log("ok try");
         console.log(result);
     }).catch(err=>{
+        console.log("error try");
         console.log(err);
     })
     ;
-var target="coaps://192.168.0.13:41283/switch";
+var target="coaps://192.168.0.13:45585/";
 var timeout=1000;
+
+/* var options={
+    keepAlive: boolean,
+    confirmable: boolean,
+    retransmit: boolean
+};
+coap
+    .request(
+        resource ,
+        method ,
+        [payload ,
+        [options ]]
+    )
+    .then(response => { })
+    .catch(err => {  })
+    ;
+ */
 coap
     .ping(
         target /* string | url | Origin */,
         [timeout /* number, time in ms */]
     )
     .then((success /* boolean */) => { 
-        console.log(success);
+        console.log("ok try");console.log(success);
         /* handle response */})
         .catch(err=>{
+            console.log("error ping");
             console.log(err);
         })
     ;
