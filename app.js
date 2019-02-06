fe9ea317534af0c0846e9f59fbe54020f8907479
fmacurio-espe <fmacurio@espe.edu.ec>
2019-02-06 01:52:12 -0500

    TryToConn

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const coap = require("node-coap-client").CoapClient;
+var hostname="localhost";
+var params={
+    psk: { 
+        "identity": "12345678-1234-1234-1234-123456789012"
+        // ... potentially more psk identities
+    }
+};
+coap.setSecurityParams(hostname /* string */, params /* SecurityParameters */);
 
+coap
+    .tryToConnect(target /* string */)
+    .then((result /* true or error code or Error instance */) => {
+        // do something with the result */ 
+        console.log(result);
+    }).catch(err=>{
+        console.log(err);
+    })
+    ;
 var target="coaps://192.168.0.13:41283/switch";
 var timeout=1000;
 coap
